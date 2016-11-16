5f4f437073d3806fb71e15c293b90c9295bce4a2
Jean-François <jfbeuve@yahoo.fr>
2016-11-16 23:08:57 +0100

    linear fading

--- webdmx/src/main/java/fr/jfbeuve/webdmx/v2/io/DmxChannel.java
+++ webdmx/src/main/java/fr/jfbeuve/webdmx/v2/io/DmxChannel.java
@@ -1,12 +1,12 @@
 package fr.jfbeuve.webdmx.v2.io;
 
 public class DmxChannel {
-	private final static int[] scale = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90,100,150,200,255};
+	private final static int[] scale = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90,100,150,200,255};
 	
 	private int startVal, endVal, dmxVal;
 	private long startTime, fadeTime, steps;
 	private int startStep;
-
+	
 	DmxChannel(int val){
 		dmxVal = val;
 		endVal = val;
@@ -26,10 +26,12 @@ public class DmxChannel {
 		steps = endStep - startStep;
 	}
 	int get(){
-
 		//no fading
 		if(dmxVal == endVal) return endVal;
 		
+		return getByLog();
+	}
+	private int getByLog(){
 		long time = System.currentTimeMillis();
 		if(time>startTime+fadeTime){
 			// fading end
@@ -37,12 +39,29 @@ public class DmxChannel {
 			return dmxVal;
 		}
 		
-		// ratio
+		// ratio by scale
+		//TODO use float?
 		int step = startStep + (int) ((time-startTime)*steps/fadeTime) ; 
 		dmxVal = scale[step];
 		
 		return dmxVal;
 	}
+	
+	private int getByLin(){
+		long time = System.currentTimeMillis();
+		if(time>startTime+fadeTime){
+			// fading end
+			dmxVal = endVal;
+			return dmxVal;
+		}
+		
+		// ratio linear 
+		//TODO use float?
+		dmxVal = startVal + (endVal-startVal) * ((int)(time-startTime)) / (int)fadeTime;
+		
+		return dmxVal;
+	}
+	
 	boolean isCompleted(){
 		return dmxVal == endVal;
 	}
