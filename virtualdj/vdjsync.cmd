2d61c82fe67518d20c723ec9d889368315b8d283
Jean-François <jfbeuve@yahoo.fr>
2016-11-18 23:01:40 +0100

    removing virtualdj from webdmx repo

--- virtualdj/vdjsync.cmd
+++
@@ -1,8 +0,0 @@
-cd D:\documents\workspace\virtualdj
-set CLASSPATH=..\libs\commons-io-1.4.jar
-set CLASSPATH=%CLASSPATH%;..\libs\log4j-1.2.15.jar
-set CLASSPATH=%CLASSPATH%;bin
-java fr.beuve.vdj.comments.MusicStoreFile canal50.properties
-pause
-java fr.beuve.vdj.title.TitleCleaner canal50.properties
-pause
